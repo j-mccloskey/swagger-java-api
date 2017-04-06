d11f621154af386ff821edfa05dffd699b7934a1
John McCloskey <johnmccloskey123@hotmail.com>
2017-04-06 10:45:35 +0100

    Bumped some versions and added maven-war-plugin to build war correctly

--- src/main/java/com/github/jmccloskey/api/impl/StatusApiServiceImpl.java
+++ src/main/java/com/github/jmccloskey/api/impl/StatusApiServiceImpl.java
@@ -11,6 +11,8 @@ import javax.ws.rs.core.SecurityContext;
 public class StatusApiServiceImpl extends StatusApiService {
     @Override
     public Response status(SecurityContext securityContext) throws NotFoundException {
-        return Response.ok().entity(new Status().status(Status.StatusEnum.OK)).build();
+        Status status = new Status();
+        status.status(Status.StatusEnum.OK).name("Swagger Java API");
+        return Response.ok().entity(status).build();
     }
 }
